61a9e63f0929ac6b3e5ba0e80131845edbb1aea2
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2025-07-10 16:08:23 -0400

    Add function to display books and filler data

--- myscript.js
+++ myscript.js
@@ -1,4 +1,25 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        id: "123e4567-e89b-12d3-a456-426614174002",
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        pages: 281,
+        read: "read",
+        info: function () {
+          console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
+        }
+      },
+      {
+        id: "123e4567-e89b-12d3-a456-426614174003",
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        pages: 180,
+        read: "unread",
+        info: function () {
+          console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
+        }
+      }
+];
 
 function Book(title, author, pages, read){
     
@@ -23,5 +44,25 @@ function addBookTolibrary(title, author, pages, read) {
 };
 
 function displayBooks() {
-    
-}
+    const container = document.querySelector(".card-container")
+
+    myLibrary.forEach((book) => {
+        const card = document.createElement("div");
+        card.className = "card"
+        container.appendChild(card);
+        
+        const title = document.createElement("div");
+        title.textContent = book.title;
+        card.appendChild(title);
+        
+        const author = document.createElement("div");
+        author.textContent = book.author;
+        card.appendChild(author);
+        
+        const pages = document.createElement("div");
+        pages.textContent = `${book.pages} pages`;
+        card.appendChild(pages);
+    });
+};
+
+displayBooks();
